823a024e98db120c4c4d6fd6965beddd0beeda8f
Luke Wong <lukewong93@gmail.com>
2023-02-04 16:19:11 +0800

    remove starter code

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <Head>
         <title>Luke Wong</title>
-        <meta name='description' content='' />
+        <meta name='description' content='my portfolio website' />
         <link rel="icon" href="../public/favicon.ico" />  
       </Head>
 
-      <div></div>
-    </main>
+      <main>
+        <section className='bg-blue-700 h-screen '>
+          <h1>Hello world</h1>
+        </section>
+      </main>
+    </div>
   )
 }
