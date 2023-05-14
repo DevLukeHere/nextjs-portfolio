0be686c458d68e53d03eb0c2bfc4b30b665ed5d9
Luke Wong <lukewong93@gmail.com>
2023-05-14 17:22:17 +0800

    style: add footer section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import "./globals.css";
 import { Lato } from "next/font/google";
 import { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+import Link from "next/link";
 
 // If loading a variable font, you don't need to specify the font weight
 const lato = Lato({
@@ -10,11 +19,7 @@ const lato = Lato({
   display: "optional",
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={lato.className}>
       {/*
@@ -22,7 +27,70 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <footer>
+          <div className="bg-teal-600 py-3.5 px-4 grid grid-rows-2 justify-items-center gap-2 content-center items-center sm:flex sm:justify-between">
+            <p className="text-gray-200 text-sm">
+              get connected with me on social networks:
+            </p>
+            <div className="grid grid-cols-4 gap-x-4">
+              <a href="#">
+                <FontAwesomeIcon
+                  className="text-gray-200 w-5 h-5 hover:text-black"
+                  icon={faGithub}
+                />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon
+                  className="text-gray-200 w-5 h-5 hover:text-black"
+                  icon={faLinkedin}
+                />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon
+                  className="text-gray-200 w-5 h-5 hover:text-black"
+                  icon={faStackOverflow}
+                />
+              </a>
+              <a href="#">
+                <FontAwesomeIcon
+                  className="text-gray-200 w-5 h-5 hover:text-black"
+                  icon={faFacebook}
+                />
+              </a>
+            </div>
+          </div>
+          <div className="bg-gray-800 grid p-5 gap-4 grid-rows-2 sm:grid-cols-2 sm:grid-rows-1">
+            <div>
+              <h4 className="text-base text-gray-200">about me</h4>
+              <p className="text-sm text-gray-200">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Consectetur nihil quas ex odio repellat? Voluptas provident ex
+                eveniet modi doloribus, ipsum laborum officiis incidunt iure
+                excepturi eaque voluptatibus tempora cum. Hic voluptatibus eum,
+                aut vitae asperiores ratione incidunt sapiente suscipit.
+              </p>
+            </div>
+            <div>
+              <h4 className="text-base text-gray-200">useful links</h4>
+              <p className="text-sm text-gray-200">Lorem, ipsum dolor.</p>
+              <p className="text-sm text-gray-200">Lorem ipsum dolor sit.</p>
+              <p className="text-sm text-gray-200">Lorem, ipsum.</p>
+              <p className="text-sm text-gray-200">Lorem.</p>
+            </div>
+          </div>
+          <div className="bg-teal-900 flex justify-center py-2 items-center gap-1">
+            <FontAwesomeIcon
+              className="text-gray-200 w-3 h-3"
+              icon={faCopyright}
+            />
+            <p className="text-gray-200 text-xs">
+              2023 Copyright: Luke Wong Zhi Kiang
+            </p>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
